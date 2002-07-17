b5be3f07520336e5ad52cd4653a45c960fa9a4d8
Benjamin Reed <rangerrick@users.sourceforge.net>
2002-07-17 01:14:28 +0000

    Can I be any more anal?  I don't think so...

--- update.sh
+++ update.sh
@@ -2,7 +2,10 @@
 
 pushd /var/ftp/pub/utilities/install/web >/dev/null 2>&1
 cvs -z3 -q update -Pd
+chown -R ranger:opennms .
 find . -name \*.sh -exec chmod 755 {} \;
 find . -name \*.pl -exec chmod 755 {} \;
 find . -name \*.mperl -exec chmod 755 {} \;
+find . -type d -exec chmod 2775 {} \;
+find . -type f -exec chmod g+w {} \;
 popd >/dev/null 2>&1
